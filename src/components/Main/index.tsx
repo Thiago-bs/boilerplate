582b1c3ee952115f04f3815eab3914c2ecf2c666
Thiago Siqueira <97thiagobs@gmail.com>
2021-04-22 21:27:49 -0300

    cosmetics on main need add more tests

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,22 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.ContainerLogo>
+      <S.Logo
+        src="/img/atomo_react.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.LogoWrapper>
+        <S.Title>Boilerplate React</S.Title>
+        <S.Author>Create by: Thiago Borges Siqueira</S.Author>
+      </S.LogoWrapper>
+    </S.ContainerLogo>
+    <S.Description>TypeScript, ReactJS e Styled Components</S.Description>
+    <S.Illustration
+      src="/img/developer.jpeg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    ></S.Illustration>
+  </S.Wrapper>
 )
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,15 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
-
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument()
-
     expect(container.firstChild).toMatchSnapshot()
   })
 })
