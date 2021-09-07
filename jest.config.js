516b12f0c395a51984d0973179f7ca6d46d1abbc
Thiago <97thiagobs@gmail.com>
2021-09-07 04:55:00 -0300

    remove test from stories

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/components/**/*.ts(x)?',
+    '!src/components/**/stories.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
